8ee891664a79312bdad3cbf6fd54824cf1c0a317
00my <maxine@highpathcapital.com>
2019-02-09 20:53:59 -0800

    trash

--- FoodCam/app/src/main/java/com/example/a/foodcam/MainActivity.java
+++ FoodCam/app/src/main/java/com/example/a/foodcam/MainActivity.java
@@ -1,11 +1,33 @@
 package com.example.a.foodcam;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.*;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telecom.Call;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +61,16 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //Intent what = new Intent(this, CallCameraActivity.class);
+        //startActivity(what);
+
+        
     }
+
+
+
+
+
 
 }
