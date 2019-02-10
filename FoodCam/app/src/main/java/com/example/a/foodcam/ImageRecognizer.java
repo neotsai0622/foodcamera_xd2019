af8876cc8cc8dfe6b5eeacaeebc77309f2b86c83
fisherxue <fisherxue0618@gmail.com>
2019-02-10 01:00:48 -0800

    working image/text recognition

--- FoodCam/app/src/main/java/com/example/a/foodcam/ImageRecognizer.java
+++ FoodCam/app/src/main/java/com/example/a/foodcam/ImageRecognizer.java
@@ -1,4 +1,50 @@
 package com.example.a.foodcam;
 
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabel;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabelDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class ImageRecognizer {
+    private static HashMap<String, Double> labelTracker = new HashMap<>();
+    public static HashMap<String, Double> runImageRecognition(Bitmap mSelectedImage) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
+
+        FirebaseVisionCloudLabelDetector detector = FirebaseVision.getInstance().getVisionCloudLabelDetector();
+
+        detector.detectInImage(image)
+                .addOnSuccessListener (
+                        new OnSuccessListener<List<FirebaseVisionCloudLabel>>() {
+                            @Override
+                            public void onSuccess(List<FirebaseVisionCloudLabel> labels) {
+                                processCloudImageLabelRecognitionResult(labels);
+                            }
+                        }
+                )
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                e.printStackTrace();
+                            }
+                        });
+        return labelTracker;
+    }
+
+    public static void processCloudImageLabelRecognitionResult(List<FirebaseVisionCloudLabel> labels) {
+        labelTracker = new HashMap<>();
+        for (FirebaseVisionCloudLabel label : labels) {
+            labelTracker.put(label.getLabel(), (double) label.getConfidence());
+        }
+    }
 }
--- FoodCam/app/src/main/java/com/example/a/foodcam/TextRecognizer.java
+++ FoodCam/app/src/main/java/com/example/a/foodcam/TextRecognizer.java
@@ -1,4 +1,134 @@
 package com.example.a.foodcam;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 public class TextRecognizer {
+    private static String text = "";
+
+    public static String runCloudTextRecognition(Bitmap mSelectedImage) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
+                .getCloudTextRecognizer();
+        recognizer.processImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                                text = processCloudTextRecognitionResult(texts);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                e.printStackTrace();
+                                text = "Failed";
+                            }
+                        });
+        return text;
+
+    }
+
+    public static String processCloudTextRecognitionResult(FirebaseVisionText text) {
+        // Task completed successfully
+        if (text == null) {
+            return "Failed"; }
+
+        StringBuilder sb = new StringBuilder();
+        List<FirebaseVisionText.TextBlock> blocks = text.getTextBlocks();
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+                    sb.append(elements.get(k).getText() + " ");
+                }
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static Bitmap getBitmapFromAsset(Context context, String filePath) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream is;
+        Bitmap bitmap = null;
+        try {
+            is = assetManager.open(filePath);
+            bitmap = BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
 }
+
+    /*
+    private void runCloudDocumentTextRecognition() {
+        mCloudButton.setEnabled(false);
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
+        FirebaseVisionDocumentTextRecognizer recognizer = FirebaseVision.getInstance()
+                .getCloudDocumentTextRecognizer();
+        recognizer.processImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionDocumentText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionDocumentText texts) {
+                                mCloudButton.setEnabled(true);
+                                processCloudDocumentTextRecognitionResult(texts);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                mCloudButton.setEnabled(true);
+                                e.printStackTrace();
+                            }
+                        });
+    }
+
+    private boolean processCloudDocumentTextRecognitionResult(FirebaseVisionDocumentText text) {
+        // Task completed successfully
+        if (text == null) {
+            showToast("No text found");
+            return false;
+        }
+        StringBuilder sb = new StringBuilder();
+        List<FirebaseVisionDocumentText.Block> blocks = text.getBlocks();
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionDocumentText.Paragraph> paragraphs = blocks.get(i).getParagraphs();
+            for (int j = 0; j < paragraphs.size(); j++) {
+                List<FirebaseVisionDocumentText.Word> words = paragraphs.get(j).getWords();
+                for (int l = 0; l < words.size(); l++) {
+                    sb.append(words.get(l).getText() + " ");
+                }
+                sb.append("\n");
+            }
+        }
+        mCloudButton.setText(sb.toString());
+        Log.v("CloudDocumentText", sb.toString());
+        return true;
+    }
+    */
