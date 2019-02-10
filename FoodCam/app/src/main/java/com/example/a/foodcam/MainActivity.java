341444590d291a246ca018c8aa9106f5e90d59d0
fisherxue <42402061+fisherxue@users.noreply.github.com>
2019-02-10 01:15:59 -0800

    cleaned up firebase code from mainactivity

--- FoodCam/app/src/main/java/com/example/a/foodcam/MainActivity.java
+++ FoodCam/app/src/main/java/com/example/a/foodcam/MainActivity.java
@@ -125,17 +125,27 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        mTextButton.setOnClickListener(new View.OnClickListener() {
+        mImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                runTextRecognition();
+                mImageButton.setEnabled(false);
+                HashMap<String, Double> map = ImageRecognizer.runImageRecognition(mSelectedImage);
+                StringBuilder sb = new StringBuilder();
+                for (String key : map.keySet()) {
+                    sb.append(key + " " +  map.get(key).toString() + "\n");
+                }
+                Log.v("img", sb.toString());
+                mImageButton.setEnabled(true);
             }
         });
 
         mCloudButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                runCloudTextRecognition();
+                mCloudButton.setEnabled(false);
+                String txt = TextRecognizer.runCloudTextRecognition(mSelectedImage);
+                Log.v("text", txt);
+                mCloudButton.setEnabled(true);
             }
         });
 
@@ -160,151 +170,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-    }
-
-    private void runTextRecognition() {
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
-        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
-                .getOnDeviceTextRecognizer();
-        mTextButton.setEnabled(false);
-        recognizer.processImage(image)
-                .addOnSuccessListener(
-                        new OnSuccessListener<FirebaseVisionText>() {
-                            @Override
-                            public void onSuccess(FirebaseVisionText texts) {
-                                mTextButton.setEnabled(true);
-                                processTextRecognitionResult(texts);
-                            }
-                        })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                // Task failed with an exception
-                                mTextButton.setEnabled(true);
-                                e.printStackTrace();
-                                mCloudButton.setText("Failure");
-                            }
-                        });
-    }
-
-    private void runCloudTextRecognition() {
-        mCloudButton.setEnabled(false);
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
-        FirebaseVisionDocumentTextRecognizer recognizer = FirebaseVision.getInstance()
-                .getCloudDocumentTextRecognizer();
-        recognizer.processImage(image)
-                .addOnSuccessListener(
-                        new OnSuccessListener<FirebaseVisionDocumentText>() {
-                            @Override
-                            public void onSuccess(FirebaseVisionDocumentText texts) {
-                                mCloudButton.setEnabled(true);
-                                processCloudTextRecognitionResult(texts);
-                            }
-                        })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                // Task failed with an exception
-                                mCloudButton.setEnabled(true);
-                                e.printStackTrace();
-                            }
-                        });
-    }
-
-    private void processCloudTextRecognitionResult(FirebaseVisionDocumentText text) {
-        // Task completed successfully
-        if (text == null) {
-            return;
-        }
-        StringBuilder sb = new StringBuilder();
-        List<FirebaseVisionDocumentText.Block> blocks = text.getBlocks();
-        for (int i = 0; i < blocks.size(); i++) {
-            List<FirebaseVisionDocumentText.Paragraph> paragraphs = blocks.get(i).getParagraphs();
-            for (int j = 0; j < paragraphs.size(); j++) {
-                List<FirebaseVisionDocumentText.Word> words = paragraphs.get(j).getWords();
-                for (int l = 0; l < words.size(); l++) {
-                    sb.append(words.get(l).getText() + " ");
-                }
-            }
-        }
-        mCloudButton.setText(sb.toString());
-        Log.v("myApp", sb.toString());
-    }
-
-
-    private void processTextRecognitionResult(FirebaseVisionText texts) {
-        List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
-        if (blocks.size() == 0) {
-            return;
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < blocks.size(); i++) {
-            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
-            for (int j = 0; j < lines.size(); j++) {
-                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
-                for (int k = 0; k < elements.size(); k++) {
-                    sb.append(elements.get(k).getText() + " ");
-                }
-            }
-        }
-        mTextButton.setText(sb.toString());
-        Log.v("myApp", sb.toString());
-    }
-
-/*
-    private Integer getImageMaxWidth() {
-        if (mImageMaxWidth == null) {
-            // Calculate the max width in portrait mode. This is done lazily since we need to
-            // wait for
-            // a UI layout pass to get the right values. So delay it to first time image
-            // rendering time.
-            mImageMaxWidth = mImageView.getWidth();
-        }
-
-        return mImageMaxWidth;
-    }
-
-    // Returns max image height, always for portrait mode. Caller needs to swap width / height for
-    // landscape mode.
-    private Integer getImageMaxHeight() {
-        if (mImageMaxHeight == null) {
-            // Calculate the max width in portrait mode. This is done lazily since we need to
-            // wait for
-            // a UI layout pass to get the right values. So delay it to first time image
-            // rendering time.
-            mImageMaxHeight =
-                    mImageView.getHeight();
-        }
-
-        return mImageMaxHeight;
-    }
-
-    private Pair<Integer, Integer> getTargetedWidthHeight() {
-        int targetWidth;
-        int targetHeight;
-        int maxWidthForPortraitMode = getImageMaxWidth();
-        int maxHeightForPortraitMode = getImageMaxHeight();
-        targetWidth = maxWidthForPortraitMode;
-        targetHeight = maxHeightForPortraitMode;
-        return new Pair<>(targetWidth, targetHeight);
-    }
-*/
-
-    public static Bitmap getBitmapFromAsset(Context context, String filePath) {
-        AssetManager assetManager = context.getAssets();
-
-        InputStream is;
-        Bitmap bitmap = null;
-        try {
-            is = assetManager.open(filePath);
-            bitmap = BitmapFactory.decodeStream(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return bitmap;
     }
 
     CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
